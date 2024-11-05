baee012d1935025609789d69499a2730cca3cc74
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2024-11-05 17:50:34 +0000

    Add event listener to button to log the value of input

---
+++ script.js
@@ -0,0 +1,27 @@
+const text = document.getElementById('text');
+const key = document.getElementById('key');
+const submitBtn = document.getElementById('submit');
+
+
+
+
+
+
+
+
+
+
+
+submitBtn.addEventListener("click", function(event){
+    event.preventDefault()
+
+    console.log(key.value)
+    console.log(text.value)
+  });
+
+  //get elements - done
+
+  //change the letters into numbers as per the position in alphabet
+
+  
+
