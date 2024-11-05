f67c60ecd0d2fa64f14aaaecd36e31e0711fbd72
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2024-11-05 18:06:55 +0000

    Convert text and key into numbers based on alphabet position

--- script.js
+++ script.js
@@ -2,26 +2,41 @@ const text = document.getElementById('text');
 const key = document.getElementById('key');
 const submitBtn = document.getElementById('submit');
 
+function keyReplacement(text){
+
+}
+
+function alphabetPosition(text) {
+    var result = "";
+    for (var i = 0; i < text.length; i++) {
+      var code = text.toUpperCase().charCodeAt(i)
+      if (code > 64 && code < 91) {
+        result += (code - 65) + " ";
+      } 
+    }
+  
+    return result
+  }
 
 
+ 
 
 
-
-
-
-
-
-
-submitBtn.addEventListener("click", function(event){
+  submitBtn.addEventListener("click", function(event){
     event.preventDefault()
 
     console.log(key.value)
     console.log(text.value)
+
+    console.log(alphabetPosition(key.value));
   });
+
 
   //get elements - done
 
-  //change the letters into numbers as per the position in alphabet
+  //change the letters into numbers as per the position in alphabet done
 
+// make the key replace every letter of the text
   
+//add numbers of both text and key modulus 26
 
