79e5a03a1060c771dd60ef143097fd86c8463652
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2024-11-05 18:48:55 +0000

    Convert text into string with no spaces, then convert into key repeated to text length

--- script.js
+++ script.js
@@ -2,14 +2,23 @@ const text = document.getElementById('text');
 const key = document.getElementById('key');
 const submitBtn = document.getElementById('submit');
 
-function keyReplacement(text){
+function keyReplacement(){
+    let originalText = text.value;
+    let keyWord = key.value
+    let removedSpacesText = originalText.split(" ").join("");
+    console.log(removedSpacesText)
+    let keyReplace = keyWord.repeat(removedSpacesText.length); 
+    console.log(keyReplace)
+    let textLength = removedSpacesText.length
+    let result = keyReplace.slice(0, textLength)
 
+    return result
 }
 
 function alphabetPosition(text) {
-    var result = "";
-    for (var i = 0; i < text.length; i++) {
-      var code = text.toUpperCase().charCodeAt(i)
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+      let code = text.toUpperCase().charCodeAt(i)
       if (code > 64 && code < 91) {
         result += (code - 65) + " ";
       } 
@@ -25,9 +34,7 @@ function alphabetPosition(text) {
   submitBtn.addEventListener("click", function(event){
     event.preventDefault()
 
-    console.log(key.value)
-    console.log(text.value)
-
+    console.log(keyReplacement())
     console.log(alphabetPosition(key.value));
   });
 
@@ -36,7 +43,7 @@ function alphabetPosition(text) {
 
   //change the letters into numbers as per the position in alphabet done
 
-// make the key replace every letter of the text
+// make the key replace every letter of the text 
   
-//add numbers of both text and key modulus 26
+//add numbers of both text and key modulus 26 
 
